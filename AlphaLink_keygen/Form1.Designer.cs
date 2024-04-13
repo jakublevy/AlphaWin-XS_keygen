200c52f7d6346e08895d1dc4c781f5fc6d10bc2b
Jakub Levý <jakub.levy15@gmail.com>
2024-04-13 14:19:35 +0200

    Initial commit

---
+++ AlphaLink_keygen/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace AlphaLink_keygen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.registrationNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.permanentRadioButton = new System.Windows.Forms.RadioButton();
+            this.trialRadioButton = new System.Windows.Forms.RadioButton();
+            this.muteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // registrationNumberTextBox
+            // 
+            this.registrationNumberTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.registrationNumberTextBox.Location = new System.Drawing.Point(12, 25);
+            this.registrationNumberTextBox.Name = "registrationNumberTextBox";
+            this.registrationNumberTextBox.Size = new System.Drawing.Size(345, 20);
+            this.registrationNumberTextBox.TabIndex = 0;
+            this.registrationNumberTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(345, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Registration Number:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.usernameTextBox.Location = new System.Drawing.Point(12, 101);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(345, 20);
+            this.usernameTextBox.TabIndex = 1;
+            this.usernameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(345, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Username:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.resultTextBox.Location = new System.Drawing.Point(12, 271);
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.Size = new System.Drawing.Size(345, 20);
+            this.resultTextBox.TabIndex = 6;
+            this.resultTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.resultTextBox.Enter += new System.EventHandler(this.resultTextBox_Enter);
+            // 
+            // generateButton
+            // 
+            this.generateButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.generateButton.Location = new System.Drawing.Point(115, 194);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(144, 52);
+            this.generateButton.TabIndex = 4;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // permanentRadioButton
+            // 
+            this.permanentRadioButton.AutoSize = true;
+            this.permanentRadioButton.Checked = true;
+            this.permanentRadioButton.Location = new System.Drawing.Point(68, 148);
+            this.permanentRadioButton.Name = "permanentRadioButton";
+            this.permanentRadioButton.Size = new System.Drawing.Size(76, 17);
+            this.permanentRadioButton.TabIndex = 2;
+            this.permanentRadioButton.TabStop = true;
+            this.permanentRadioButton.Text = "Permanent";
+            this.permanentRadioButton.UseVisualStyleBackColor = true;
+            this.permanentRadioButton.CheckedChanged += new System.EventHandler(this.radiobutton_CheckedChanged);
+            // 
+            // trialRadioButton
+            // 
+            this.trialRadioButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.trialRadioButton.AutoSize = true;
+            this.trialRadioButton.Location = new System.Drawing.Point(241, 148);
+            this.trialRadioButton.Name = "trialRadioButton";
+            this.trialRadioButton.Size = new System.Drawing.Size(62, 17);
+            this.trialRadioButton.TabIndex = 3;
+            this.trialRadioButton.Text = "90 days";
+            this.trialRadioButton.UseVisualStyleBackColor = true;
+            this.trialRadioButton.CheckedChanged += new System.EventHandler(this.radiobutton_CheckedChanged);
+            // 
+            // muteButton
+            // 
+            this.muteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.muteButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.muteButton.Location = new System.Drawing.Point(287, 185);
+            this.muteButton.Name = "muteButton";
+            this.muteButton.Size = new System.Drawing.Size(70, 70);
+            this.muteButton.TabIndex = 5;
+            this.muteButton.UseVisualStyleBackColor = true;
+            this.muteButton.Click += new System.EventHandler(this.muteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 303);
+            this.Controls.Add(this.muteButton);
+            this.Controls.Add(this.trialRadioButton);
+            this.Controls.Add(this.permanentRadioButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.registrationNumberTextBox);
+            this.MinimumSize = new System.Drawing.Size(385, 342);
+            this.Name = "Form1";
+            this.Text = "AlphaLink keygen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox registrationNumberTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.RadioButton permanentRadioButton;
+        private System.Windows.Forms.RadioButton trialRadioButton;
+        private System.Windows.Forms.Button muteButton;
+    }
+}
+
---
+++ AlphaLink_keygen/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AlphaLink_keygen.Properties;
+using NAudio.Wave;
+
+namespace AlphaLink_keygen
+{
+    public partial class Form1 : Form
+    {
+        private string Md5Salt;
+        private string trialSalt = "SlbjMF%%uB!eS8nYV8aOg65v0Wdb9w8kHeBysVnT7Wjowzz&sbi!9JXrCX#Rqb6uT*JZUoj@r^D8HVAwU*ZTe1wY8X7p6qXDUys9n4r0kU1Q7A^TqTbUSDI5cltxRBHL65^NzTAlv!75nO%aGnkhY7FRba9$ESrSvNC#*8Bo9lnxaTGdY8N#obWTLh&x5!Jg5gWLdzT*jNPK4jSwK9ZVxf96xujmaeuzU^3u^twItFEetKIVcAj!cM^Hv&";
+        private string permanentSalt = "IK0c*D2nxZlFvEn@KYSt3P%9yBFEUme&8t4Une$J$zG3rFQdpryM$zYUp%ti^9EBV0bfDCipxPWM&suz&QISHeTbqcMGA9adG!Ng1VNyR8xor@BkBq2TmkLG5lvv&O@1i@3AavpH$YP9Vjy2CU^x$MNMaLtvV&h@dax7EmANTJdithg4CHDDVkUPu5HGYwKId$o2SSjO@YVgOYNSsBb#GoMsJxqgVZT&xd*v!G#Rs6fdaGIJGSWCJcWwb4";
+
+        private bool muted = false;
+        MemoryStream ms = new MemoryStream(Resources.music);
+        private Mp3FileReader reader;
+        WaveOut waveOut = new WaveOut(); // or WaveOutEvent()
+        public Form1()
+        {
+            InitializeComponent();
+            Icon = Resources.icon;
+            Md5Salt = permanentSalt;
+            UpdateVolumeButton();
+            MusicInit();
+        }
+
+        private void UpdateVolumeButton()
+        {
+            if (muted)
+                muteButton.BackgroundImage = Resources.music_off;
+            
+            else
+                muteButton.BackgroundImage = Resources.music_on;
+            
+        }
+
+        private void MusicInit()
+        {
+            reader = new Mp3FileReader(ms);
+            waveOut.Init(reader);
+            waveOut.PlaybackStopped += (sender, args) =>
+            {
+                reader.Seek(0, SeekOrigin.Begin);
+                waveOut.Play();
+            };
+            waveOut.Play();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string toHash = registrationNumberTextBox.Text + usernameTextBox.Text + Md5Salt;
+            MemoryStream ms = new MemoryStream(Encoding.ASCII.GetBytes(toHash));
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] a = md5.ComputeHash(ms);
+            string encoded = BitConverter.ToString(a).Replace("-", string.Empty).ToUpper();
+            char last = encoded.Last();
+            for (int i = 1; i <= 4; i++)
+            {
+                encoded = encoded.Insert(i * 4 + i - 1, "-");
+            }
+
+            encoded = encoded.Insert(23, last.ToString());
+            encoded = encoded.Substring(0, 24);
+            resultTextBox.Text = encoded;
+        }
+
+        private void resultTextBox_Enter(object sender, EventArgs e)
+        {
+            if(resultTextBox.Text != "")
+                Clipboard.SetText(resultTextBox.Text);
+        }
+
+        private void radiobutton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton which = (RadioButton) sender;
+            if (which.Text == "Permanent")
+                Md5Salt = permanentSalt;
+            
+            else
+                Md5Salt = trialSalt;
+            
+        }
+
+        private void muteButton_Click(object sender, EventArgs e)
+        {
+            muted = !muted;
+
+            waveOut.Volume = muted ? 0 : 1;
+            UpdateVolumeButton();
+        }
+    }
+}
